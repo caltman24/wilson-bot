055e7a773521e9bd4fa724af036d3f3e768199ad
Corbyn Altman <corbyn.altman01@gmail.com>
2022-12-22 20:08:46 -0500

    added change to dist

--- dist/api/fetchQuote.js
+++ dist/api/fetchQuote.js
@@ -7,7 +7,7 @@ const axios_1 = __importDefault(require("axios"));
 const fetchRandomQuote = async () => {
     const res = (0, axios_1.default)({
         method: "get",
-        url: "https://owen-wilson-wow-api.herokuapp.com/wows/random",
+        url: "https://owen-wilson-wow-api.onrender.com/wows/random",
     });
     const { data } = await res;
     return data[0];
